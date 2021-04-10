60c4f22c04371674737c0e8dc29b52ec319f2d7f
Michael Jeppesen <mj@lpj-software.dk>
2021-04-10 12:20:48 +0200

    Initial add of cloud solution

---
+++ LightAutomation/src/main/java/dk/dtuIoTgrp6/LightAutomation/LightAutomationApplication.java
@@ -0,0 +1,13 @@
+package dk.dtuIoTgrp6.LightAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightAutomationApplication.class, args);
+	}
+
+}
---
+++ LightAutomation/src/test/java/dk/dtuIoTgrp6/LightAutomation/LightAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.dtuIoTgrp6.LightAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
