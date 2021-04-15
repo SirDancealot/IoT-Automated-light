8a2568799d61cf941fd7b69d1c9b6fe6100c2490
Michael Jeppesen <mj@lpj-software.dk>
2021-04-15 09:34:58 +0200

    renamed folder of the web api

---
+++ AutomatedLight web api/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomatedLight.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<DataItem> DataItems { get; set; }
+    }
+}
