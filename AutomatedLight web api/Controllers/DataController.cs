c4f8c60aad4d605b6aa58505ba7d81cef04e6000
Michael Jeppesen <mj@lpj-software.dk>
2021-04-15 10:33:41 +0200

    Added three endpoints to the api to get the latest temp/lux/humidity individually

--- AutomatedLight web api/Controllers/DataController.cs
+++ AutomatedLight web api/Controllers/DataController.cs
@@ -35,6 +35,26 @@ namespace AutomatedLight.Controllers
             return items.Max();
         }
 
+        [HttpGet("latest/temp")]
+        public async Task<ActionResult<double>> GetLatestTemp()
+        {
+            ActionResult<DataItem> item = await GetLatest();
+            return item.Value.Temp;
+        }
+        [HttpGet("latest/lux")]
+        public async Task<ActionResult<double>> GetLatestLux()
+        {
+            ActionResult<DataItem> item = await GetLatest();
+            return item.Value.Lux;
+        }
+        [HttpGet("latest/humidity")]
+        public async Task<ActionResult<double>> GetLatestHumidity()
+        {
+            ActionResult<DataItem> item = await GetLatest();
+            return item.Value.Humidity;
+        }
+
+
         // POST: Data?temp=<value>&lux=<value>&humidity=<value>
         [HttpPost]
         public async Task<ActionResult<DataItem>> PostData(double temp, double lux, double humidity)
