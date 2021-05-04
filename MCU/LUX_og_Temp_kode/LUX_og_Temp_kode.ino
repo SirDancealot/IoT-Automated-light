30cb423b7914fbfad02dcee6e9d7b4c07ebd0d68
Mahdi Mohamad <mahdimohamed3@gmail.com>
2021-05-04 14:34:26 +0200

    http fejl kode

--- MCU/LUX_og_Temp_kode/LUX_og_Temp_kode.ino
+++ MCU/LUX_og_Temp_kode/LUX_og_Temp_kode.ino
@@ -3,40 +3,35 @@
 #include <ESP8266WiFiMulti.h>   // Include the Wi-Fi-Multi library
 #include <ESP8266WebServer.h>   // Include the WebServer library
 #include <ESP8266mDNS.h>        // Include the mDNS library
-#include <DHT.h>
-#define DHTTYPE DHT11 
-DHT dht(5, DHTTYPE);
-
 
 ESP8266WiFiMulti wifiMulti;
 
+#include <DHT.h>                // Include library for the temp and humidity
+#define DHTTYPE DHT11 
+DHT dht(5, DHTTYPE);
 
 //Wifi id and password
 const char* ssid = "iphone";
 const char* password = "12345678";
 
 //Set light source pin
-const byte led = 4;
-
-
+const byte LED = 2; //D4 pin
 const char* serverName = "http://api.mj-software.dk/data";
-
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
-float temp, lux;
+float temp = 0;
+int lux = 0;
 float voltage = 0;
 float humidity = 0;
-
 const int LDR = A0; //analog signal
-int inputValue;
 
 void setup()  {
-  Serial.begin(9600);
-  dht.begin();
-Serial.println("Temperatur and humidity\n\n");
-delay(500);  
- // pinMode(cSelectPin, OUTPUT); //sætter pin D8 som output
-  wifiMulti.addAP(ssid, password);
+Serial.begin(9600);
+pinMode(LED, OUTPUT);
+Serial.println("Temperatur, Humidity & Lux\n\n");
+delay(100);
+
+ wifiMulti.addAP(ssid, password);
   Serial.println("Connecting");
   while(wifiMulti.run() != WL_CONNECTED) {
     delay(500);
@@ -45,9 +40,9 @@ delay(500);
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-
-  pinMode(led,OUTPUT);
-  digitalWrite(led,LOW);
+  pinMode(LED, OUTPUT); 
+  digitalWrite(LED,LOW);
+  dht.begin();
 }
 
 void loop()
@@ -58,33 +53,35 @@ void loop()
     //Wifi
     if(WiFi.status()== WL_CONNECTED){
       HTTPClient http;
-      
-       
+
   
+//Code for temp (DHT11)
 temp = dht.readTemperature();
 Serial.print("Temperature = ");
-Serial.print(String(temp) + "\t"); //printer værdi ud
-Serial.println(" C  ");
+Serial.print(temp);
+Serial.println(" C°  ");
 delay(300);
 
+//Code for humidity (DHT11)
 humidity = dht.readHumidity();
 Serial.print("Humidity = ");
-Serial.print(String(humidity) + "\t"); //printer værdi ud
-Serial.println(" %  ");
+Serial.print(humidity);
+Serial.println(" %\n ");
 delay(300);
 
-lux = analogRead(LDR);//read the input on analog pin 0
-voltage = lux*(3.3/1023.0); //Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
-Serial.print(String(voltage) + "\t"); //printer værdi ud
-
-
-    
+//Code for Lux (LDR sensor)
+lux = analogRead(A0);//read the input on analog pin 0
+voltage = lux*(3.3/1023); //Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 3.3V)
+Serial.print("Lux = ");
+Serial.print(voltage);// print out the value you read
+Serial.println(" V\n  ");
+analogWrite(LED, lux);
+ 
   Serial.println();
   
   delay(100); //delay med 100 ms
-
       
-      // Specify content-type header
+          // Specify content-type header
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
       // Data to send with HTTP POST
       char *httpPostData;
@@ -93,10 +90,10 @@ Serial.print(String(voltage) + "\t"); //printer værdi ud
       http.begin(serverName + String(httpPostData));
       // Send HTTP POST request
       int httpResponseCode = http.POST("");
-     
+
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
-        
+
       // Free resources
       http.end();
     } else {
@@ -105,8 +102,8 @@ Serial.print(String(voltage) + "\t"); //printer værdi ud
 
     // Send http GET -----------------------------
     HTTPClient httpget;
-    
-    String serverPath = serverName + "/latest/lux";
+
+    String serverPath = serverName + String("/latest/lux");
 
     httpget.begin(serverPath.c_str());
 
@@ -123,12 +120,12 @@ Serial.print(String(voltage) + "\t"); //printer værdi ud
         Serial.println(httpResponseCode);
       }
 
-      if(lux<500){
-        digitalWrite(led,HIGH);
-      } else {
-        digitalWrite(led,LOW);
-      }
-    
+//      if(lux<500){
+//        digitalWrite(led,HIGH);
+//      } else {
+//        digitalWrite(led,LOW);
+//      }
+
     lastTime = millis();
   }
 }
